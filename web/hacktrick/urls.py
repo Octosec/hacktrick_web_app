8e90517a5bc7001b977e61d6904f51537c287522
Mazlum Ağar <info@mazlumagar.com>
2017-03-18 23:08:41 +0300

    bug fix

--- web/hacktrick/urls.py
+++ web/hacktrick/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url('^faq/$', FAQListView.as_view(), name='faq'),
     url('^cfp/$', CFPView.as_view(), name='cfp'),
     url('^trainings/$', TrainingListView.as_view(), name='trainings'),
-    url(r'^trainings/(?P<pk>[0-9]+)$', TrainingDetailView.as_view(), name='training_detail'),
+    url(r'^trainings/(?P<pk>[0-9]{1,10})$', TrainingDetailView.as_view(), name='training_detail'),
     url(r'^contributors/$', ContributorListView.as_view(), name='contributors'),
 ]
