6e4e3739c2ac33e42a75686f2538d191d3c81626
mazlum ağar <mazlum.agar@gmail.com>
2017-12-17 13:18:40 +0300

    issue #31, email send task has been fixed

--- web/hacktrick/tasks.py
+++ web/hacktrick/tasks.py
@@ -9,12 +9,18 @@ def send_email_for_information(email_type, email_to, extra):
     try:
         from .models import Mail
         mail_object = Mail.objects.get(type=email_type)
+
         content = mail_object.content + extra
-        content += "<br/><br/><a href='http://www.hacktrickconf.com'>http://www.hacktrickconf.com</a>"
+        content += "<br/><br/><a href='http://www.hacktrickconf.com'>" \
+                   "http://www.hacktrickconf.com</a>"
         content += "<br/>Hacktrick Organizasyon Ekibi"
         subject, email_from = mail_object.title, "noreply@hacktrickconf.com"
-        mail_send = EmailMultiAlternatives(mail_object.title, content, email_from, email_to[0], bcc=email_to[1:])
-        mail_send.attach_alternative(content, "text/html")
-        mail_send.send()
+        for m in email_to:
+            mail_send = EmailMultiAlternatives(
+                mail_object.title, content, email_from, m)
+            mail_send.attach_alternative(content, "text/html")
+            mail_send.send()
+
     except Exception as e:
-        logging.error('There was some error when sending email: {}'.format(e), exc_info=True)
+        logging.error('There was some error when sending email: {}'.format(e),
+                      exc_info=True)
